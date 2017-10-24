d9e6a498fc7ec12cde3bb920bc51a43dad1d5f1c
Dominique McCloud <niquelejon@gmail.com>
2017-10-24 23:10:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ $(document).ready(function (){
   req.send()
 }
 });
+
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const src = document.getElementById("repository-template").innerHTML
+  const template = Handlebars.compile(src)
+  const repoList = template(repos)
+  document.getElementById("results").innerHTML = repoList
+}
