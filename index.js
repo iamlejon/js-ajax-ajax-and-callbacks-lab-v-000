8625cccbef526af2ea5acdce5dd51af8f0b4170e
Dominique McCloud <niquelejon@gmail.com>
2017-10-24 23:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
   function searchRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/search/repositories?q=${searchTerms')
+  req.open("GET", 'https://api.github.com/search/repositories?q=${searchTerms}')
   req.send()
 }
 });
